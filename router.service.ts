0dba6fbb4b458ad64ce893d1e9bf1ec85a3738dc
Chacaflús <davidpemo36@hotmail.com>
2025-10-21 11:04:56 +0200

    Create router.service.ts

---
+++ router.service.ts
@@ -0,0 +1,44 @@
+import { envHandler } from "./env.handler";
+import { pingHandler } from "./ping.handler";
+import { RouterOptionsType, RouteType } from "src/types";
+
+export class RouterService {
+  routes: RouteType[];
+
+  constructor(
+    readonly routesIn: RouteType[],
+    readonly options: RouterOptionsType
+  ) {
+    this.routes = [
+      {
+        method: "GET",
+        path: "/ping",
+        handler: pingHandler,
+        description: "Checks if lambda is alive",
+      },
+      {
+        method: "GET",
+        path: "/check-env",
+        handler: checkEnvHandler,
+        description: "Checks environment variables",
+      },
+      ...routesIn,
+    ];
+  }
+
+  getRoutes() {
+    return this.routes;
+  }
+
+  getRoutesWithStage() {
+    const fixedRoutes = this.routes.map((o) => {
+      return {
+        ...o,
+        path: ["", this.options.apiName, "{stage}", o.path.substring(1)].join(
+          "/"
+        ),
+      };
+    });
+    return fixedRoutes;
+  }
+}
